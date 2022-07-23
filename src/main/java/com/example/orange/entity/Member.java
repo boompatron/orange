ffa40f484d7093d837d3b90282274a3b4506de0b
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-07-23 22:59:58 +0900

    DB는 가장 익숙한 mysql 사용할 얘정 나중에는 mariaDB로 바꾸고 해볼듯 현재는 간단하게 로컬이랑 연결하고 테이블 생성되는거 확인

--- src/main/java/com/example/orange/entity/Member.java
+++ src/main/java/com/example/orange/entity/Member.java
@@ -1,9 +1,40 @@
 package com.example.orange.entity;
 
+import com.example.orange.constant.Role;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.IdentityHashMap;
 
+@Entity
+@Table(name = "member")
+@Getter
+@NoArgsConstructor
 public class Member extends BaseEntity {
+    @Id
+    @Column(name = "member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private Role role;
+
+    @Column
+    private String picture;
+
+    @Builder
+    public Member(String name, String email, String picture, Role role){
+        this.email = email;
+        this.name = name;
+        this.picture = picture;
+        this.role = role;
+    }
 }
