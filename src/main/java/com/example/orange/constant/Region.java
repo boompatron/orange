f9be1479744e63d125b41af7b708f1e7c4fab0af
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-07-24 23:13:00 +0900

    지역 일단 추가

---
+++ src/main/java/com/example/orange/constant/Region.java
@@ -0,0 +1,9 @@
+package com.example.orange.constant;
+
+public enum Region {
+    Seoul, GyeonggiDo,
+    GyeongsangDo, GangwonDo,
+    ChungcheongDo,
+    JeollaDo,JejuIsland,
+    Busan, Daegu, Incheon, Gwangju, Daejeon
+}
