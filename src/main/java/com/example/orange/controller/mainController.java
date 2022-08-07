1901431618571bbac9fd86dadf958e8cfb00a697
Bosub Kim <39071638+boompatron@users.noreply.github.com>
2022-08-07 23:39:47 +0900

    footer 랑 header가 잘 되넹.. 아직 시도중

--- src/main/java/com/example/orange/controller/mainController.java
+++ src/main/java/com/example/orange/controller/mainController.java
@@ -1,4 +1,23 @@
 package com.example.orange.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
 public class mainController {
+    @GetMapping(value = "/")
+    public String main(){
+        return "aaa";
+    }
+    @GetMapping(value = "/footer")
+    public String footer(){
+        return "fragments/footer";
+    }
+
+    @GetMapping(value = "/header")
+    public String header(){
+        return "fragments/header";
+    }
 }
